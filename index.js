472301f8990e5a6184093a38dd0bfd04be7bd212
Paul Tobin <tobster111@gmail.com>
2019-01-17 11:46:12 +0000

    Authenication calls sucessfull

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const handlebars = require('handlebars');
 require('dotenv').config();
 
 // Define our constants, you will change these with your own
-const TWITCH_CLIENT_ID = '<YOUR CLIENT ID HERE>';
-const TWITCH_SECRET = '<YOUR CLIENT SECRET HERE>';
-const SESSION_SECRET = '<SOME SECRET HERE>';
-const CALLBACK_URL = '<YOUR REDIRECT URL HERE>'; // You can run locally with - http://localhost:3000/auth/twitch/callback
+// const TWITCH_CLIENT_ID = '<YOUR CLIENT ID HERE>';
+// const TWITCH_SECRET = '<YOUR CLIENT SECRET HERE>';
+// const SESSION_SECRET = '<SOME SECRET HERE>';
+// const CALLBACK_URL = '<YOUR REDIRECT URL HERE>'; // You can run locally with - http://localhost:3000/auth/twitch/callback
 
 const {
   TWITCH_CLIENT_ID,
@@ -51,3 +51,77 @@ OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
     }
   });
 };
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(
+  'twitch',
+  new OAuth2Strategy(
+    {
+      authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+      tokenURL: 'https://id.twitch.tv/oauth2/token',
+      clientID: TWITCH_CLIENT_ID,
+      clientSecret: TWITCH_SECRET,
+      callbackURL: CALLBACK_URL,
+      state: true
+    },
+    function(accessToken, refreshToken, profile, done) {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+
+      // Securely store user profile in your DB
+      //User.findOrCreate(..., function(err, user) {
+      //  done(err, user);
+      //});
+
+      done(null, profile);
+    }
+  )
+);
+
+// Set route to start OAuth link, this is where you define scopes to request
+app.get(
+  '/auth/twitch',
+  passport.authenticate('twitch', { scope: 'user_read' })
+);
+
+// Set route for OAuth redirect
+app.get(
+  '/auth/twitch/callback',
+  passport.authenticate('twitch', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  })
+);
+
+// Define a simple template to safely generate HTML with values from user's profile
+var template = handlebars.compile(`
+<html><head><title>Twitch Auth Sample</title></head>
+<table>
+    <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
+    <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
+    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
+    <tr><th>Bio</th><td>{{bio}}</td></tr>
+    <tr><th>Image</th><td>{{logo}}</td></tr>
+</table></html>`);
+
+// If user has an authenticated session, display it, otherwise display link to authenticate
+app.get('/', function(req, res) {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    res.send(template(req.session.passport.user));
+  } else {
+    res.send(
+      '<html><head><title>Twitch Auth Sample</title></head><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png"></a></html>'
+    );
+  }
+});
+
+app.listen(3000, function() {
+  console.log('Twitch auth sample listening on port 3000!');
+});
